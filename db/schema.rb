ab306c91d0528f04d459375e4311e91af656d59b
Nagai_sho <as.uland.dr@gmail.com>
2024-11-16 17:16:52 +0900

    モデルにconfirmableの追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_11_16_041741) do
+ActiveRecord::Schema[7.0].define(version: 2024_11_16_073229) do
   create_table "emails", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "content", null: false
     t.datetime "created_at", null: false
@@ -25,6 +25,11 @@ ActiveRecord::Schema[7.0].define(version: 2024_11_16_041741) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
+    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
